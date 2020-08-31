2682b21e1becfa65832c80b5e3ef5675fe1a7660
Emerson Ford <emersontford@gmail.com>
2020-08-30 23:51:55 -0600

    Add autogen and configure steps to build-client.sh

--- build-client.sh
+++ build-client.sh
@@ -1,12 +1,17 @@
 cd libraries/libmlx4-1.2.1mlnx1/
+./autogen.sh
+./configure --prefix=/usr/ --libdir=/usr/lib/ --sysconfdir=/etc/
 make
 make install
 
 cd ../libibverbs-1.2.1mlnx1/
+./autogen.sh
+./configure --prefix=/usr/ --libdir=/usr/lib/ --sysconfdir=/etc/
 make
 make install
 
 cd ../librdmacm-1.1.0mlnx/
+./autogen.sh
+./configure --prefix=/usr/ --libdir=/usr/lib/ --sysconfdir=/etc/
 make
 make install
-
