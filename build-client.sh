022f1808a8c93ce2a1dff6c2102676f2c448c394
Emerson Ford <emersontford@gmail.com>
2020-09-07 22:23:43 -0600

    Add libmempool build step

--- build-client.sh
+++ build-client.sh
@@ -1,4 +1,8 @@
-cd libraries/libmlx4-1.2.1mlnx1/
+cd libmempool
+make
+make install
+
+cd ../libraries/libmlx4-1.2.1mlnx1/
 ./autogen.sh
 ./configure --prefix=/usr/ --libdir=/usr/lib/ --sysconfdir=/etc/
 make
